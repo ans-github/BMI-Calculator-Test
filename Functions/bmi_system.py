c5a3c9d3d5f0fc2518d8de9763563732c4d3da28
ans-github <anas.alfakhuri@gmail.com>
2020-11-30 13:43:23 +0000

    readme file modified, bmi system file modified

--- Functions/bmi_system.py
+++ Functions/bmi_system.py
@@ -6,7 +6,7 @@ def bmi_calc_metric():
     my_input2 = int(input("please enter your height in cm: "))
     bmi_result_met = my_input / my_input2 / my_input2 * 10000
     print("Your BMI is " + str(bmi_result_met))
-    input("Please press any key to return to exit program...")
+    input("Please press any key to exit program...")
 
 def bmi_calc_imperial():
     my_input = int(input("Please enter your weight in lbs: "))
@@ -15,4 +15,4 @@ def bmi_calc_imperial():
     bmi_height = my_input2 * my_input2
     bmi_result_imp = bmi_height / bmi_weight
     print("Your BMI is " + str(bmi_result_imp))
-    input("Please press any key to return to exit program...")
+    input("Please press any key to exit program...")
